0fe16526d67bb1d3731cad2f5460633719652142
Glynis Monroe <Glynis.d.Monroe@gmail.com>
2018-04-17 07:22:10 +0000

    linked user_id to post_id

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,30 +1,37 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
-
-  # GET /posts
-  # GET /posts.json
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :correct_user, only: [:edit, :update, :destroy]
+  
+  respond_to :html
+  
   def index
     @posts = Post.all
   end
 
-  # GET /posts/1
-  # GET /posts/1.json
+ 
   def show
+    respond_with(@post)
   end
 
-  # GET /posts/new
+
   def new
-    @post = Post.new
+    @post = current_user.posts.build
   end
 
-  # GET /posts/1/edit
+ 
   def edit
   end
 
-  # POST /posts
-  # POST /posts.json
+ 
   def create
-    @post = Post.new(post_params)
+    @post = current_user.posts.build(post_params)
+   if @post.save
+    redirect_to @post, notice: 'Post was sucessfully created.'
+   else
+    render action: 'new'
+   end
+  end
 
     respond_to do |format|
       if @post.save
@@ -32,43 +39,36 @@ class PostsController < ApplicationController
         format.json { render :show, status: :created, location: @post }
       else
         format.html { render :new }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
       end
     end
-  end
-
-  # PATCH/PUT /posts/1
-  # PATCH/PUT /posts/1.json
+  
   def update
-    respond_to do |format|
-      if @post.update(post_params)
-        format.html { redirect_to @post, notice: 'Post was successfully updated.' }
-        format.json { render :show, status: :ok, location: @post }
+    if @post.update(post_params)
+        redirect_to @post, notice: 'Post was sucessfully updated'.
       else
-        format.html { render :edit }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
-      end
+        render action: 'edit'
     end
   end
-
-  # DELETE /posts/1
-  # DELETE /posts/1.json
+  
   def destroy
     @post.destroy
-    respond_to do |format|
-      format.html { redirect_to posts_url, notice: 'Post was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to posts_url
   end
+  
 
-  private
-    # Use callbacks to share common setup or constraints between actions.
+private
     def set_post
       @post = Post.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:body)
+      params.required(:post).permit(:body)
     end
+
+  def correct_user
+      @post = current_user.posts.find_by(id: params[:id])
+      redirect_to posts_path, notice: "Not authorized to edit this post." if @post.nil?
+  
+  end
+
 end
