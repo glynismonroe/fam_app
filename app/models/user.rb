13ccd1a775fcc61d817a1a4b0c4975c7c5621aaa
Terry Robinson <Terrance.n.robinson@gmail.com>
2018-04-23 23:55:48 -0400

    tidyed up some views

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,28 +1,28 @@
 class User < ActiveRecord::Base
   acts_as_messageable
-  
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :posts
-  
+
 end
 
  def name
-   "User #{id}"
+   "User #{:id}"
  end
- 
+
  def mailboxer_email(object)
    nil
  end
- 
-def new 
+
+def new
     @recipients = User.all - [current_user]
 end 
 
-def create 
+def create
     recipient = User.find(params[:user_id])
     receipt = current_user.send_message(recipient, params[:body], params[:subject])
     redirect_to conversation_path(receipt.conversation)
-end 
+end
